0a3a23100d7def37733958c45427c5a407cac95d
Nourhan <nmohamed@tendegrees.sa>
2021-02-09 01:10:38 +0200

    Create model and table for mails and mail attachments

---
+++ database/migrations/2021_02_08_224033_create_mail_attachments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mail_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('email_id');
+            $table->string("name");
+            $table->string("file_path");
+            $table->foreign('email_id')->references('id')->on('mails');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mail_attachments');
+    }
+}
