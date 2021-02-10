269825f88ea8427766d6cb11fad2a11bc3387efc
Nourhan <mnourrhan@gmail.com>
2021-02-10 23:04:44 +0200

    - Integrate with redis package for queuing mails through redis. - Integrate with horizon package for monitoring queue. - Update queue connection to redis. - Handle queuing emails through redis in the SendEmail job.

--- app/Jobs/SendEmail.php
+++ app/Jobs/SendEmail.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Redis;
 
 class SendEmail implements ShouldQueue
 {
@@ -36,7 +37,13 @@ class SendEmail implements ShouldQueue
      */
     public function handle()
     {
-        $email = new MailService($this->details, $this->attachments);
-        Mail::to($this->details['receiver_email'])->send($email);
+        // Allow only 2 emails every 1 second
+        Redis::throttle('mail-service')->allow(2)->every(1)->then(function () {
+            $email = new MailService($this->details, $this->attachments);
+            Mail::to($this->details['receiver_email'])->send($email);
+        }, function () {
+            // Could not obtain lock; this job will be re-queued
+            $this->release(2);
+        });
     }
 }
